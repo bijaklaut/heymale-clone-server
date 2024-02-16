dc225e6bec5d19946e5c1089f02052eb7a86896f
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-02-16 19:12:22 +0700

    Adding timestamps

--- app/order/model.js
+++ app/order/model.js
@@ -1,71 +1,74 @@
 const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-aggregate-paginate-v2");
 
-const orderSchema = mongoose.Schema({
-   invoice: {
-      type: String,
-      unique: true,
-      required: [true, "Transaction Invoice is required"],
-   },
-   user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: [true, "User is required"],
-   },
-   orderItem: [
-      {
-         _id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-         },
-         item_name: {
-            type: String,
-            required: [true, "Product Name is required"],
-         },
-         quantity: {
-            type: Number,
-            required: [true, "Quantity is required"],
-         },
-         price: {
-            type: Number,
-            required: [true, "Price is required"],
-         },
+const orderSchema = mongoose.Schema(
+   {
+      invoice: {
+         type: String,
+         unique: true,
+         required: [true, "Transaction Invoice is required"],
       },
-   ],
-   status: {
-      type: String,
-   },
-   shippingDetail: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Shipment",
-   },
-   transaction: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Transaction",
-      required: [true, "Transaction is required"],
-   },
-   voucher: {
-      voucher_id: {
+      user: {
          type: mongoose.Schema.Types.ObjectId,
-         ref: "Voucher",
+         ref: "User",
+         required: [true, "User is required"],
       },
-      value: {
+      orderItem: [
+         {
+            _id: {
+               type: mongoose.Schema.Types.ObjectId,
+               ref: "Product",
+            },
+            item_name: {
+               type: String,
+               required: [true, "Product Name is required"],
+            },
+            quantity: {
+               type: Number,
+               required: [true, "Quantity is required"],
+            },
+            price: {
+               type: Number,
+               required: [true, "Price is required"],
+            },
+         },
+      ],
+      status: {
+         type: String,
+      },
+      shippingDetail: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Shipment",
+      },
+      transaction: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Transaction",
+         required: [true, "Transaction is required"],
+      },
+      voucher: {
+         voucher_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Voucher",
+         },
+         value: {
+            type: Number,
+         },
+      },
+      shippingFee: {
          type: Number,
+         required: [true, "Price is required"],
+      },
+      price: {
+         type: Number,
+         required: [true, "Price is required"],
+      },
+      totalPrice: {
+         type: Number,
+         required: [true, "Total Price is required"],
       },
    },
-   shippingFee: {
-      type: Number,
-      required: [true, "Price is required"],
-   },
-   price: {
-      type: Number,
-      required: [true, "Price is required"],
-   },
-   totalPrice: {
-      type: Number,
-      required: [true, "Total Price is required"],
-   },
-});
+   { timestamps: true }
+);
 
 orderSchema.plugin(mongoosePaginate);
 
