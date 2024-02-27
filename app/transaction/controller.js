05b932810299d43f6270cb99811d4329aec52be8
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-02-28 06:19:16 +0700

    adding get transaction endpoint

--- app/transaction/controller.js
+++ app/transaction/controller.js
@@ -3,6 +3,64 @@ const Transaction = require("./model");
 const axios = require("axios").default;
 
 module.exports = {
+   getTransactions: async (req, res) => {
+      try {
+         // const {filter, search} = req.body
+         const { p = 0 } = req.query;
+         let options = {
+            pagination: false,
+         };
+
+         if (p) {
+            options = {
+               ...options,
+               pagination: true,
+               page: p,
+               limit: 10,
+            };
+         }
+
+         const aggregate = Transaction.aggregate([
+            {
+               $sort: { transaction_time: -1 },
+            },
+         ]);
+
+         const transactions = await Transaction.aggregatePaginate(
+            aggregate,
+            options
+         );
+
+         let responseData = {
+            status: 200,
+            payload: transactions,
+            message: "Successfully get all transactions",
+         };
+
+         if (!transactions.docs.length) {
+            responseData = {
+               status: 404,
+               message: "Transaction not found",
+            };
+         }
+
+         return res.status(responseData.status).send(responseData);
+      } catch (error) {
+         let responseData = {
+            status: 500,
+            message: "Internal Server Error",
+         };
+
+         if (error.message) {
+            responseData = {
+               status: 400,
+               message: error.message,
+            };
+         }
+
+         return res.status(responseData.status).send(responseData);
+      }
+   },
    createTransaction: async (req, res) => {
       try {
          const newTransaction = await Transaction.create(req.body);
--- app/transaction/router.js
+++ app/transaction/router.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { cancelTransaction, createTransaction } = require("./controller");
+const {
+   cancelTransaction,
+   createTransaction,
+   getTransactions,
+} = require("./controller");
 // const { isLogin } = require("../middleware/auth");
 const multer = require("multer");
 
 const upload = multer();
 
+router.post("/", upload.none(), getTransactions);
 router.post("/create", upload.none(), createTransaction);
 router.post("/:order_id/cancel", upload.none(), cancelTransaction);
 
